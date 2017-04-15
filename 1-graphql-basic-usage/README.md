a2b14ed01b39e751c0811d0098b3d1d0b66c29c3
bengrunfeld <benjamin.grunfeld@gmail.com>
2017-04-14 21:19:32 -0700

    Update readme and comments

--- 1-graphql-basic-usage/README.md
+++ 1-graphql-basic-usage/README.md
@@ -1,5 +1,16 @@
-# GraphQL Examples 
+# GraphQL Example: 1 GraphQL Basic Usage 
 
-This repo contains several folders that all illustrate a different aspect of using GraphQL, in the attempt to isolate particular interactions with the Query Language that produce specific results.
+In this example, we set up an client Express server and a GraphQL Express server. Wh
 
+## How to run the example
+
+In a terminal window:
+
+    node src/js/app
+
+In *another* terminal window: 
+
+    node src/js/graphql-server.js
+
+Then in your browser, navigate to `localhost:3000`. You should see "Hello from the GraphQL Server!" appear on the page, loaded by the EJS template.
 
--- 1-graphql-basic-usage/src/js/app.js
+++ 1-graphql-basic-usage/src/js/app.js
@@ -1,24 +1,24 @@
-// Node Express GraphQL Server
+// Client requests data from GraphQL server
+
 const request = require('request')
 const express = require('express')
-const events = require('events')
-const emitter = new events.EventEmitter()
 const app = express()
 
+// Set up EJS templates
 app.set('view engine', 'ejs')
 app.set('views', 'src/templates')
 
-app.use(express.static('./src'))
-
-
-
-
-
+// On request to localhost:3000, query GraphQL
+// server and render the response to the template
 app.get('/', (req, res) => {
-  let refresh = (body) => {
-    res.render('index', {'data': body})
+  
+  // Render method
+  let refresh = (rawData) => {
+    let data = JSON.parse(rawData).data
+    res.render('index', {'data': data.greet})
   }
 
+  // Set request options
   const options = {  
     url: 'http://localhost:4000/gimme-data',
     method: 'POST',
@@ -30,51 +30,11 @@ app.get('/', (req, res) => {
     body: JSON.stringify({query: "{ greet }"})
   }
 
-  request(options, function(err, res, body) {  
+  // Issue request
+  request(options, function(err, res, body) {
     refresh(body)
   })
-
-
-  //res.render('index', { 'data': 'Initial page load' })
 })
-
-  // $.ajax({
-  //   type: 'POST',
-  //   url: 'localhost:3000/gimme-data',
-  //   //contentType: 'application/json'
-  //   dataType: 'json',
-  //   data: {"query": "greet"},
-  //   success: (data) => {
-  //     res.render('index', { 'data': data })
-  //   },
-  //   error: (xhr, statusText, error) => {
-  //     console.log('Oh fuck....')
-  //     console.log(xhr, statusText, error)
-  //   }
-  // })
-
-  // $('h1').text('boooo')
-  // let xhr = new XMLHttpRequest();
-  // xhr.responseType = 'json';
-  // xhr.open("POST", "locahost:3000/gimme-data", true);
-  // xhr.setRequestHeader("Content-Type", "application/json");
-  // xhr.setRequestHeader("Accept", "application/json");
-  // xhr.onload = function (e) {
-  //   if (xhr.readyState === 4) {
-  //     if (xhr.status === 200) {
-  //       res.render('index', { 'data': xhr.response })
-  //     } else {
-  //       console.error(xhr.statusText);
-  //     }
-  //   }
-  // }
-  // xhr.onerror = function (e) {
-  //   console.error(xhr.statusText);
-  // }
-
-  // xhr.send(JSON.stringify({query: "{ greet }"}));
-
-
 
 let server = app.listen(3000, () => {
   console.log('Listening on port 3000...')
